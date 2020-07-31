87bed862946345c5e6eeb5b142fd5e1850cf3aec
gimyeonghun <67149286+gimyeonghun@users.noreply.github.com>
2020-07-31 15:14:57 +1000

    now this should be universal

--- init.el
+++ init.el
@@ -2,11 +2,16 @@
 (tool-bar-mode -1)
 (setq backup-directory-alist
       `(("." . ,(concat user-emacs-directory "backups"))))
+(setq auto-save-default nil)
 (scroll-bar-mode -1)
 (global-linum-mode 1)
 (setq inhibit-startup-message t
       inhibit-startup-echo-area-message t)
 (add-to-list 'load-path "~/.emacs.d/elpa")
+
+(if (eq system-type 'darwin)
+    (setq journal-path "~/Documents/journal")
+  (setq journal-path "something-else"))
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 (setq make-backup-files nil)
@@ -29,99 +34,42 @@
 
 (use-package try
   :ensure t)
-             
-(require 'evil)
-(evil-mode 1)
-(require 'org-journal)
-(require 'undo-tree)
-(global-undo-tree-mode)
 
-(require 'ivy)
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
+(use-package evil
+  :config
+  (evil-mode 1))
 
-(autoload 'markdown-mode "markdown-mode"
-   "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(use-package org-journal
+  :ensure t
+  :config
+  (setq org-journal-dir journal-path))
 
-(autoload 'gfm-mode "markdown-mode"
-   "Major mode for editing GitHub Flavored Markdown files" t)
-(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+(use-package undo-tree
+  :commands global-undo-tree-mode)
 
-(autoload
-  'ace-jump-mode
-  "ace-jump-mode"
-  "Emacs quick move minor mode"
-  t)
-(define-key evil-normal-state-map (kbd "SPC") 'ace-jump-mode)
+(use-package ivy
+  :demand t
+  :custom
+  (ivy-use-virtual-buffers t)
+  (enable-recursive-minibuffers t))
 
+(use-package markdown-mode
+  :mode (("README\\.md\\'" . gfm-mode)
+	 ("\\.markdown\\'" . markdown-mode)
+	 ("\\.md\\'" . markdown-mode)))
 
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(use-package ace-jump-mode
+  :defer t
+  :bind (:map evil-normal-state-map
+	      ("SPC" . ace-jump-mode)))
 
-(autoload 'tern-mode "tern.el" nil t)
-(add-hook 'js-mode-hook (lambda () (tern-mode t)))
+(use-package web-mode
+  :mode (("\\.html?\\'" . web-mode)))
 
-(require 'yasnippet)
-(setq yas-snippet-dirs
-      '("~/.emacs.d/snippets"))
-(yas-global-mode 1)
+(use-package yasnippet
+  :demand t
+  :config
+  (yas-global-mode 1))
 
-(require 'taskpaper-mode)
-(add-to-list 'auto-mode-alist
-             '("\\.todo\\'" . taskpaper-mode))
-
-(defun applescript-quote-string (argument)
-  "Quote a string for passing as a string to AppleScript."
-  (if (or (not argument) (string-equal argument ""))
-      "\"\""
-    ;; Quote using double quotes, but escape any existing quotes or
-    ;; backslashes in the argument with backslashes.
-    (let ((result "")
-          (start 0)
-          end)
-      (save-match-data
-        (if (or (null (string-match "[^\"\\]" argument))
-                (< (match-end 0) (length argument)))
-            (while (string-match "[\"\\]" argument start)
-              (setq end (match-beginning 0)
-                    result (concat result (substring argument start end)
-                                   "\\" (substring argument end (1+ end)))
-                    start (1+ end))))
-        (concat "\"" result (substring argument start) "\"")))))
-
-(defun send-region-to-omnifocus (beg end)
-  "Send the selected region to OmniFocus.
-Use the first line of the region as the task name and the second
-and subsequent lines as the task note."
-  (interactive "r")
-  (let* ((region (buffer-substring-no-properties beg end))
-         (match (string-match "^\\(.*\\)$" region))
-         (name (substring region (match-beginning 1) (match-end 1)))
-         (note (if (< (match-end 0) (length region))
-                   (concat (substring region (+ (match-end 0) 1) nil) "\n\n")
-                 "")))
-    (do-applescript
-     (format "set theDate to current date
-              set taskName to %s
-              set taskNote to %s
-              set taskNote to (taskNote) & \"Added from Emacs on \" & (theDate as string)
-              tell front document of application \"OmniFocus\"
-                make new inbox task with properties {name:(taskName), note:(taskNote)}
-              end tell"
-             (applescript-quote-string name)
-             (applescript-quote-string note)))))
-
-(global-set-key (kbd "C-c o") 'send-region-to-omnifocus)
-
-(require 'applescript-mode)
-
+(use-package taskpaper-mode
+  :mode (("\\.todo\\'" . taskpaper-mode)))
