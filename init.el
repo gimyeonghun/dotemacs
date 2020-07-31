1e1e7c2c40189f53cd536c7282858f3518b2b206
gimyeonghun <67149286+gimyeonghun@users.noreply.github.com>
2020-07-31 14:10:45 +1000

    going to the big change

--- init.el
+++ init.el
@@ -18,10 +18,17 @@
 (define-key (cdr ido-minor-mode-map-entry) [remap write-file] nil)
 
 (require 'package)
-
+(setq package-enable-at-startup nil)
 (add-to-list 'package-archives
 	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package try
+  :ensure t)
              
 (require 'evil)
 (evil-mode 1)
@@ -118,17 +125,3 @@ and subsequent lines as the task note."
 
 (require 'applescript-mode)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (taskpaper-mode yasnippet-snippets yasnippet web-mode company ace-jump-mode tern use-package org-roam org-ref org-journal markdown-mode magit evil deft))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
