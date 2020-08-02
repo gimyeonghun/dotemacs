9ef9b342c1507a7616619f74f0822d37f786adf8
gimyeonghun <67149286+gimyeonghun@users.noreply.github.com>
2020-08-02 19:56:53 +1000

    Update init.el

--- init.el
+++ init.el
@@ -11,7 +11,9 @@
 
 (if (eq system-type 'darwin)
     (setq journal-path "~/Documents/journal")
-  (setq journal-path "C:\\Users\\gim\\Documents\\journal"))
+  (setq journal-path "C:\\Users\\gim\\Documents\\journal")
+  (add-to-list 'default-frame-alist '(font . "Consolas"))
+  (set-face-attribute 'default t :font "Consolas"))
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 (setq make-backup-files nil)
@@ -46,7 +48,9 @@
   (setq org-journal-date-format "%A, %d %B %Y"))
 
 (use-package undo-tree
-  :commands global-undo-tree-mode)
+  :ensure t
+  :init t
+  (global-undo-tree-mode)
 
 (use-package ivy
   :demand t
@@ -60,7 +64,7 @@
 	 ("\\.md\\'" . markdown-mode)))
 
 (use-package ace-jump-mode
-  :defer t
+  :ensure t
   :bind (:map evil-normal-state-map
 	      ("SPC" . ace-jump-mode)))
 
@@ -74,17 +78,3 @@
 
 (use-package taskpaper-mode
   :mode (("\\.todo\\'" . taskpaper-mode)))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (company yasnippet ivy evil-visual-mark-mode org-journal try use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
